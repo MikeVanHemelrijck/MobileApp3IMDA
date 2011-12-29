7a5dd76d54055f86af1f47562b04b3b5d71c50ff
Bram Van Houcke <bramvanhoucke@hotmail.com>
2011-12-29 16:16:28 +0100

    twitter app

--- Resources/app.js
+++ Resources/app.js
@@ -1,66 +1,21 @@
-// eh mike, kunde gij dees lezen!?
-// this sets the background color of the master UIView (when there are no windows/tab groups on it)
-Titanium.UI.setBackgroundColor('#000');
-
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
-
-
-//
-// create base UI tab and root window
-// TEST
-//
-var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
-});
-var tab1 = Titanium.UI.createTab({  
-    icon:'KS_nav_views.png',
-    title:'Tab 1',
-    window:win1
-});
-
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
-
-
-
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
-
-
-// open tab group
-tabGroup.open();
+// Project ----------------------------------------------------------------------------------------
+    // Creates a tab group with Titanium.UI API.  
+    var tabGroup = Titanium.UI.createTabGroup();  
+      
+    // Create the window "mainWin"  
+    var mainWin = Titanium.UI.createWindow ({  
+        title: "@mobtuts", // Set the title  
+        backgroundColor: "#fff", // Set the background color to white  
+        url: "tweets.js"// Link to file which will handle the code for the window  
+    });  
+      
+    // Create the tab "mainTab"  
+    var mainTab = Titanium.UI.createTab ({  
+        title: "Twitter", // Title of the tab: "Twitter"  
+        icon: "KS_nav_mashup.png", // Icon for tab, Included in the source files  
+        window: mainWin // We will create the window "mainWin"  
+    });  
+      
+    // Add the tab to our tab group  
+    tabGroup.addTab(mainTab);  
+    tabGroup.open();   
